124396f8043858c64d760f42ead6f8a740350aac
mjkonkel <mitchell.konkel@gmail.com>
2022-12-06 21:55:26 -0600

    added posts to homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['title', 'content'],
+          attributes: ['username'],
         },
       ],
     });
@@ -18,10 +18,11 @@ router.get('/', async (req, res) => {
     const posts = postData.map((post) => post.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      posts, 
-      logged_in: req.session.logged_in 
-    });
+    res.render('homepage', {posts});
+    // res.render('homepage', { 
+    //   posts, 
+    //   logged_in: req.session.logged_in 
+    // });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -35,6 +36,16 @@ router.get('/login', (req, res) => {
     // }
   
     res.render('login');
+  });
+
+  router.get('/signup', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    // if (req.session.logged_in) {
+    //   res.redirect('/profile');
+    //   return;
+    // }
+  
+    res.render('signup');
   });
 
   module.exports = router
