d97d725b2e4c3117ddeabd7dec6bc83912260c36
mjkonkel <mitchell.konkel@gmail.com>
2022-12-07 23:01:22 -0600

    setting up to view one post

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -48,4 +48,10 @@ router.get('/login', (req, res) => {
     res.render('signup');
   });
 
+  router.get('/post/:id', withAuth, async (req, res) => {
+    res.render('post', { 
+      logged_in: req.session.logged_in 
+    });
+})
+
   module.exports = router
